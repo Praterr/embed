e04927a03a6fbd2058ce30b155b836b141c7dca2
Praterr <npc7036@gmail.com>
2025-11-19 12:19:16 +0530

    hh

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def health():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print(f"EMBEDDING SERVICE STARTING ON PORT {port}")
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port,debug=True)
